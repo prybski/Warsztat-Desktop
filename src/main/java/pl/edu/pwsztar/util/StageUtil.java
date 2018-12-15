8543246cf1d44b100fad3edd983dfa5604cf04ac
prybski <p.rybski@outlook.com>
2018-12-15 19:40:27 +0100

    optymalizacja importów bibliotek

--- src/main/java/pl/edu/pwsztar/util/StageUtil.java
+++ src/main/java/pl/edu/pwsztar/util/StageUtil.java
@@ -1,16 +1,12 @@
 package pl.edu.pwsztar.util;
 
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
-import java.io.IOException;
 
 public class StageUtil {
 
